91b1d4d93d1dcfd990b7b975f2ffbb0fdd8efc3b
Brenda <brendacassita@gmail.com>
2022-03-23 10:42:52 -0600

    models setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+d1 = Physician.create(name:"Dr Gill")
+d2 = Physician.create(name:"Dr Lisa")
+
+p1 = Patient.create(name:"Sniffly Sally")
+p2 = Patient.create(name:"Ill Bill")
+p3 = Patient.create(name:"Sicky Stevey")
+
+Appointment.create(date:'June 1', physician_id:d1.id, patient_id:p1.id)
+Appointment.create(date:'May 2', physician_id:d1.id, patient_id:p1.id)
+
+Appointment.create(date:'July 2', physician_id:d2.id, patient_id:p2.id)
+Appointment.create(date:'June 3', physician_id:d2.id, patient_id:p1.id)
